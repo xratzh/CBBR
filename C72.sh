cba12d8d7aeeae314859081dc80912359b10f86e
Xrat Zhou <xratzh@gmail.com>
2017-10-05 00:51:40 +0800

    Update C72.sh

--- C72.sh
+++ C72.sh
@@ -21,7 +21,7 @@ sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
 sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr_powered" >> /etc/sysctl.conf
-lsmod |grep -q 'bbr_powered'
+lsmod |grep bbr_powered
 [ $? -eq '0' ] && {
 sysctl -p >/dev/null 2>&1
 echo "Finish! "
