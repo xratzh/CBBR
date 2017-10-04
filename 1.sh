2046f1686bb4107944ccb67094f5475aad180a7b
Xrat Zhou <xratzh@gmail.com>
2017-10-04 23:57:37 +0800

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+rpm -import http://mirror.rc.usf.edu/RPM-GPG-KEY-elrepo.org
+yum install -y http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el7/x86_64/RPMS/kernel-ml-4.11.8-1.el7.elrepo.x86_64.rpm
+grub2-set-default 0
+reboot
