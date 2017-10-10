9b1ace18886894864b143c021a29000b52ce9e16
Xrat Zhou <xratzh@gmail.com>
2017-10-10 18:12:26 +0800

    fix grub setting

--- C61.sh
+++ C61.sh
@@ -5,7 +5,7 @@ export PATH
 
 rpm -import http://mirror.rc.usf.edu/RPM-GPG-KEY-elrepo.org
 yum install -y http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el6/x86_64/RPMS/kernel-ml-4.11.8-1.el6.elrepo.x86_64.rpm
-grub2-set-default 0
+sed -i "s/default=1/default=0/g" /etc/grub.conf
 echo
 read -p "Info: The system needs to be restart. Do you want to reboot? [y/n]" is_reboot
 if [[ ${is_reboot} == "y" || ${is_reboot} == "Y" ]]; then
