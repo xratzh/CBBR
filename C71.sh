2b8c8652d503d42086e871ccd5bd619b16473a91
Xrat Zhou <xratzh@gmail.com>
2017-10-30 16:18:17 +0800

    Update C71.sh

--- C71.sh
+++ C71.sh
@@ -6,6 +6,9 @@ export PATH
 # yum install -y http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el7/x86_64/RPMS/kernel-ml-4.11.8-1.el7.elrepo.x86_64.rpm
 yum install -y yum install -y https://raw.githubusercontent.com/xratzh/CBBR/master/kernel7/kernel-ml-4.11.8-1.el7.elrepo.x86_64.rpm
 grub2-set-default 0
+
+sed -i '/\[main]/a\exclude=kernel*' /etc/yum.conf # 防止内核由于update产生变动
+
 echo
 read -p "Info: The system needs to be restart. Do you want to reboot? [y/n]" is_reboot
 if [[ ${is_reboot} == "y" || ${is_reboot} == "Y" ]]; then
