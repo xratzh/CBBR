a83ee49885a489a1138f73c53765f698393621f6
Xrat Zhou <xratzh@gmail.com>
2017-11-10 17:19:17 +0800

    Update C61.sh

--- C61.sh
+++ C61.sh
@@ -5,8 +5,7 @@ export PATH
 
 # yum install -y http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el6/x86_64/RPMS/kernel-ml-4.11.8-1.el6.elrepo.x86_64.rpm
 yum install -y https://raw.githubusercontent.com/xratzh/CBBR/master/kernel6/kernel-ml-4.11.8-1.el6.elrepo.x86_64.rpm
-sed -i "s/default=1/default=0/g" /boot/grub/grub.conf
-sed -i '/\[main]/a\exclude=kernel*' /etc/yum.conf   # 防止内核由于update产生变动
+
 echo
 read -p "Info: The system needs to be restart. Do you want to reboot? [y/n]" is_reboot
 if [[ ${is_reboot} == "y" || ${is_reboot} == "Y" ]]; then
