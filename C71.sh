63c49b1905a59d45308c650e4757a73ef303e3b4
Xrat Zhou <xratzh@gmail.com>
2017-11-25 21:44:44 +0800

    Update C71.sh

--- C71.sh
+++ C71.sh
@@ -4,7 +4,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 # yum install -y http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el7/x86_64/RPMS/kernel-ml-4.11.8-1.el7.elrepo.x86_64.rpm
-yum install -y yum install -y https://raw.githubusercontent.com/xratzh/CBBR/master/kernel7/kernel-ml-4.11.8-1.el7.elrepo.x86_64.rpm
+yum install -y https://raw.githubusercontent.com/xratzh/CBBR/master/kernel7/kernel-ml-4.11.8-1.el7.elrepo.x86_64.rpm
 grub2-set-default 0
 echo
 read -p "Info: The system needs to be restart. Do you want to reboot? [y/n]" is_reboot
