1bd757e506ea922e72ce19a996c83beef5a834e2
Xrat Zhou <xratzh@gmail.com>
2017-11-25 22:01:40 +0800

    Update D1.sh

--- D1.sh
+++ D1.sh
@@ -4,7 +4,8 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-apt-get install -y http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10.8/linux-image-4.10.8-041008-generic_4.10.8-041008.201703310531_amd64.deb
+wget -O linux-image-4.10.8.deb http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10.8/linux-image-4.10.8-041008-generic_4.10.8-041008.201703310531_amd64.deb
+dpkg -i linux-image-4.10.8.deb
 sudo update-grub
 
 echo
