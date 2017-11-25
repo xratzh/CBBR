2d5e19611bb178ed1da91aa084006b4261ca8688
Xrat Zhou <xratzh@gmail.com>
2017-11-25 21:47:56 +0800

    Create D1.sh

---
+++ D1.sh
@@ -0,0 +1,16 @@
+
+#! /bin/bash
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+apt-get install -y http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10.8/linux-image-4.10.8-041008-generic_4.10.8-041008.201703310531_amd64.deb
+sudo update-grub
+
+echo
+read -p "Info: The system needs to be restart. Do you want to reboot? [y/n]" is_reboot
+if [[ ${is_reboot} == "y" || ${is_reboot} == "Y" ]]; then
+    reboot
+else
+    exit
+fi
