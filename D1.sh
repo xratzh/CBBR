62a3acd8423e073327c5e9d27d54709b9c065b03
Xrat Zhou <xratzh@gmail.com>
2017-11-26 09:11:32 +0800

    Update D1.sh

--- D1.sh
+++ D1.sh
@@ -7,7 +7,7 @@ export PATH
 wget -O linux-image-4.10.8.deb http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10.8/linux-image-4.10.8-041008-generic_4.10.8-041008.201703310531_amd64.deb
 dpkg -i linux-image-4.10.8.deb
 rm linux-image-4.10.8.deb
-sudo update-grub
+update-grub && update-grub2
 
 echo
 read -p "Info: The system needs to be restart. Do you want to reboot? [y/n]" is_reboot
