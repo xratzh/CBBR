7b21ee96d5f60d9b7b2abb00b7331a6eee88b807
Xrat Zhou <xratzh@gmail.com>
2017-11-26 11:45:04 +0800

    Update C62.sh

--- C62.sh
+++ C62.sh
@@ -4,6 +4,14 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 [ "$EUID" -ne '0' ] && echo "Error,This script must be run as root! " && exit 1
+
+KernelList="$(rpm -qa |grep 'kernel' |awk '{print $1}')"
+[ -z "$(echo $KernelList |grep -o kernel-ml-4.11.8-1.el6.elrepo.x86_64)" ] && echo "Install error." && exit 1
+for KernelTMP in `echo "$KernelList"`
+ do
+  [ "$KernelTMP" != "kernel-ml-4.11.8-1.el6.elrepo.x86_64" ] && echo -ne "Uninstall Old Kernel\n\t$KernelTMP\n" && yum remove "$KernelTMP" -y >/dev/null 2>&1
+done
+
 yum remove kernel-headers -y
 # yum install -y http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el6/x86_64/RPMS/kernel-ml-headers-4.11.8-1.el6.elrepo.x86_64.rpm
 # yum install -y http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el6/x86_64/RPMS/kernel-ml-devel-4.11.8-1.el6.elrepo.x86_64.rpm
