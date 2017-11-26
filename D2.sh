7eb42f93eaa7f4ab878ef76fa4795611fe8475a4
Xrat Zhou <xratzh@gmail.com>
2017-11-26 10:14:16 +0800

    Update D2.sh

--- D2.sh
+++ D2.sh
@@ -6,20 +6,20 @@ export PATH
 [ "$EUID" -ne '0' ] && echo "Error,This script must be run as root! " && exit 1
 
 KernelList="$(dpkg -l |grep 'linux-image' |awk '{print $2}')"
-[ -z "$(echo $KernelList |grep -o linux-image-4.10.8-041008-generic)" ] && echo "Install error." && exit 1
+[ -z "$(echo $KernelList |grep -o linux-image-4.11.8-041008-generic)" ] && echo "Install error." && exit 1
 for KernelTMP in `echo "$KernelList"`
  do
-  [ "$KernelTMP" != "linux-image-4.10.8-041008-generic" ] && echo -ne "Uninstall Old Kernel\n\t$KernelTMP\n" && apt-get purge "$KernelTMP" -y >/dev/null 2>&1
+  [ "$KernelTMP" != "linux-image-4.11.8-041008-generic" ] && echo -ne "Uninstall Old Kernel\n\t$KernelTMP\n" && apt-get purge "$KernelTMP" -y >/dev/null 2>&1
 done
 
-apt-mark hold linux-image-4.10.8-041008-generic # 锁定内核版本
+apt-mark hold linux-image-4.11.8-041108-generic # 锁定内核版本
 
-wget -O linux-headers-4.10.8.deb http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10.8/linux-headers-4.10.8-041008_4.10.8-041008.201703310531_all.deb
-dpkg -i linux-headers-4.10.8.deb
+wget -O linux-headers-4.11.8.deb http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.11.8/linux-headers-4.11.8-041108_4.11.8-041108.201706290836_all.deb
+dpkg -i linux-headers-4.11.8.deb
 
-wget -O linux-headers-generic-4.10.8.deb http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10.8/linux-headers-4.10.8-041008-generic_4.10.8-041008.201703310531_amd64.deb
-dpkg -i linux-headers-generic-4.10.8.deb
-rm linux-headers-4.10.8.deb linux-headers-generic-4.10.8.deb
+wget -O linux-headers-generic-4.11.8.deb http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.11.8/linux-headers-4.11.8-041108-generic_4.11.8-041108.201706290836_amd64.deb
+dpkg -i linux-headers-generic-4.11.8.deb
+rm linux-headers-4.11.8.deb linux-headers-generic-4.11.8.deb
 which gcc >/dev/null 2>&1
 [ $? -ne '0' ] && {
 echo "Install gcc..."
