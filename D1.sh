8e29e8876e801f76666e49803f9f1e6ee80c402d
Xrat Zhou <xratzh@gmail.com>
2017-11-26 09:25:25 +0800

    Update D1.sh

--- D1.sh
+++ D1.sh
@@ -4,6 +4,8 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
+
+[ "$EUID" -ne '0' ] && echo "Error,This script must be run as root! " && exit 1
 wget -O linux-image-4.10.8.deb http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10.8/linux-image-4.10.8-041008-generic_4.10.8-041008.201703310531_amd64.deb
 dpkg -i linux-image-4.10.8.deb
 rm linux-image-4.10.8.deb
