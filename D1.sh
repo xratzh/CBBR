5317ac4df52f83a65b9610d605fbc8ad1111505e
Xrat Zhou <xratzh@gmail.com>
2018-01-29 19:56:34 +0800

    Update D1.sh

--- D1.sh
+++ D1.sh
@@ -6,7 +6,7 @@ export PATH
 
 
 [ "$EUID" -ne '0' ] && echo "Error,This script must be run as root! " && exit 1
-wget -O linux-image-4.11.8.deb https://raw.githubusercontent.com/xratzh/CBBR/master/debkernel/linux-image-4.11.8-041108-generic_4.11.8-041108.201706290836_amd64.deb
+wget --no-check-certificate -O linux-image-4.11.8.deb https://raw.githubusercontent.com/xratzh/CBBR/master/debkernel/linux-image-4.11.8.deb
 dpkg -i linux-image-4.11.8.deb
 rm linux-image-4.11.8.deb
 update-grub && update-grub2
